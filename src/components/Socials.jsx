e3a0714c52a05d8d10eaa98bd5ebfa2c39f028b2
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-04 20:08:43 +0100

    styled socials component

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import {FaFacebookSquare, FaInstagramSquare, FaLinkedin} from "react-icons/fa"
+
 const Socials = () => {
   return (
-    <div>Socials</div>
+    <div className='flex gap-2'>
+      <FaFacebookSquare size={25} fill="black"/>
+      <FaInstagramSquare size={25} fill="black"/>
+      <FaLinkedin size={25} fill="black"/>
+    </div>
   )
 }
 
