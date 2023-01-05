90aee5cf8b4435378f91f726dfb159988bbfaed7
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-05 19:57:33 +0100

    styled socials component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
 const Navbar = () => {
+  let links = [
+    {
+      name:"Home",
+    },
+    {
+      name:"IT",
+    },
+    {
+      name:"Clothes",
+    },
+    {
+      name:"Gadgets",
+    },
+    {
+      name:"Contact",
+    },
+  ]
   return (
-    <nav>Navbar</nav>
+    <nav className='flex font-bold gap-6'>
+      {links.map(link=>(
+        <div className='cursor-pointer'>{link.name}</div>
+      ))}
+    </nav>
   )
 }
 
