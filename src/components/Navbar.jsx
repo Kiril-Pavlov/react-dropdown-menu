1f8bfe77fbb2e96d2db2b0dbc6ed8a74fa1d59cd
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-06 22:02:27 +0100

    reafactor links in another file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,20 @@
 import React from "react";
 
+import { links } from "../data/fakeData";
+
 const Navbar = () => {
-  let links = [
-    {
-      name: "Home",
-    },
-    {
-      name: "IT",
-      submenu: true,
-      sublinks: [
-        {
-          head: "PC",
-          sublinkArray: [
-            { name: "Gaming", link: "/" },
-            { name: "Work", link: "/" },
-            { name: "Regular", link: "/" },
-            { name: "Used", link: "/" },
-          ],
-        },
-        {
-          head: "Monitors",
-          sublinkArray: [
-            { name: "Gaming", link: "/" },
-            { name: "Work", link: "/" },
-            { name: "Regular", link: "/" },
-            { name: "Used", link: "/" },
-          ],
-        },
-        {
-          head: "Phones",
-          sublinkArray: [
-            { name: "Android", link: "/" },
-            { name: "IOS", link: "/" },
-            { name: "Other", link: "/" },
-          ],
-        },
-        {
-          head: "Printers",
-          sublinkArray: [
-            { name: "Color", link: "/" },
-            { name: "Black and White", link: "/" },
-            { name: "Industry", link: "/" },
-          ],
-        },
-      ],
-    },
-    {
-      name: "Clothes",
-    },
-    {
-      name: "Gadgets",
-    },
-    {
-      name: "Contact",
-    },
-  ];
+  
   return (
-    <nav className="flex gap-5">
+    <nav className="flex gap-10">
       {links.map((link) => (
-        <div  className="md:cursor-pointer px-4 py-1 text-left font-bold hover:bg-slate-50 group">
-          <div>
+        <div  className="md:cursor-pointer py-1 hover:bg-slate-50 group">
+          <div className="font-bold px-4">
             {link.name}
           </div>
           <div>
             {link.submenu && (
               <div className="">
-                <div className="absolute top-10 bg-orange-300 p-3 hidden group-hover:block hover:block">
+                <div className="absolute top-10 bg-orange-300 p-4 hidden group-hover:block hover:block">
                   <div className="grid grid-cols-3 gap-5">
                     {link.sublinks.map((sublink) => (
                       <div>
---
+++ src/data/fakeData.jsx
@@ -0,0 +1,54 @@
+export const links = [
+    {
+      name: "Home",
+    },
+    {
+      name: "IT",
+      submenu: true,
+      sublinks: [
+        {
+          head: "PC",
+          sublinkArray: [
+            { name: "Gaming", link: "/" },
+            { name: "Work", link: "/" },
+            { name: "Regular", link: "/" },
+            { name: "Used", link: "/" },
+          ],
+        },
+        {
+          head: "Monitors",
+          sublinkArray: [
+            { name: "Gaming", link: "/" },
+            { name: "Work", link: "/" },
+            { name: "Regular", link: "/" },
+            { name: "Used", link: "/" },
+          ],
+        },
+        {
+          head: "Phones",
+          sublinkArray: [
+            { name: "Android", link: "/" },
+            { name: "IOS", link: "/" },
+            { name: "Other", link: "/" },
+          ],
+        },
+        {
+          head: "Printers",
+          sublinkArray: [
+            { name: "Color", link: "/" },
+            { name: "Black and White", link: "/" },
+            { name: "Industry", link: "/" },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Clothes",
+    },
+    {
+      name: "Gadgets",
+    },
+    {
+      name: "Contact",
+    },
+];
