584b6cda12f363a755d7dff28fbb80ee7eef0695
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-06 21:25:35 +0100

    positioning the dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,65 +1,87 @@
-import React from 'react'
-
+import React from "react";
 
 const Navbar = () => {
   let links = [
     {
-      name:"Home",
+      name: "Home",
     },
     {
-      name:"IT",
-      submenu:true,
-      sublinks:[
+      name: "IT",
+      submenu: true,
+      sublinks: [
         {
-          head:"PC",
-          sublinkArray:[
-            {name:"Gaming",link:'/'},
-            {name:"Work",link:'/'},
-            {name:"Regular",link:'/'},
-            {name:"Used",link:'/'},
-          ]
-        }
-      ]
+          head: "PC",
+          sublinkArray: [
+            { name: "Gaming", link: "/" },
+            { name: "Work", link: "/" },
+            { name: "Regular", link: "/" },
+            { name: "Used", link: "/" },
+          ],
+        },
+        {
+          head: "Monitors",
+          sublinkArray: [
+            { name: "Gaming", link: "/" },
+            { name: "Work", link: "/" },
+            { name: "Regular", link: "/" },
+            { name: "Used", link: "/" },
+          ],
+        },
+        {
+          head: "Phones",
+          sublinkArray: [
+            { name: "Android", link: "/" },
+            { name: "IOS", link: "/" },
+            { name: "Other", link: "/" },
+          ],
+        },
+        {
+          head: "Printers",
+          sublinkArray: [
+            { name: "Color", link: "/" },
+            { name: "Black and White", link: "/" },
+            { name: "Industry", link: "/" },
+          ],
+        },
+      ],
     },
     {
-      name:"Clothes",
+      name: "Clothes",
     },
     {
-      name:"Gadgets",
+      name: "Gadgets",
     },
     {
-      name:"Contact",
+      name: "Contact",
     },
-  ]
+  ];
   return (
-    <nav className='flex font-bold gap-2'>
-      {links.map(link=>(
-        <>
-          <div className='cursor-pointer border-4 px-4 py-1'>{link.name}</div>
-          {link.submenu && (
-            <div>
-              <div className='absolute top-10 border-2'>
-                <div>
-                  {link.sublinks.map(sublink=>(
+    <nav className="flex gap-5">
+      {links.map((link) => (
+        <div>
+          <div className="cursor-pointer px-4 py-1 text-left font-bold hover:bg-slate-50">{link.name}</div>
+          <div>
+            {link.submenu && (
+              <div className="absolute top-10 bg-orange-300 p-3">
+                <div className="grid grid-cols-3 gap-10">
+                  {link.sublinks.map((sublink) => (
                     <div>
-                      <div>{sublink.head}</div>
+                      <div className="font-bold">{sublink.head}</div>
                       <div>
-                        {sublink.sublinkArray.map(item=>(
-                          <div>
-                            {item.name}
-                          </div>
+                        {sublink.sublinkArray.map((item) => (
+                          <div>{item.name}</div>
                         ))}
                       </div>
                     </div>
                   ))}
                 </div>
               </div>
-            </div>
-          )}
-        </>
+            )}
+          </div>
+        </div>
       ))}
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
